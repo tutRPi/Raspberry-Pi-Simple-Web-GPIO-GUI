9c56bc2a1c097e85815ee78e7803451d73953e3c
Benoît Arnoult <benoit.a@skello.io>
2021-10-01 11:56:14 +0200

    blink logic

--- routes/ajax.js
+++ routes/ajax.js
@@ -24,6 +24,29 @@ module.exports = function(req, res) {
                 res.send({ gpio: req.body.gpio, status: req.body.status });
             
             break;
+            case 'blink':
+                /*
+ * Set the initial state to low.  The state is set prior to the pin
+ * being actived, so is safe for devices which require a stable setup.
+ */
+            rpio.open(16, rpio.OUTPUT, rpio.LOW);
+
+            /*
+            * The sleep functions block, but rarely in these simple programs does
+            * one care about that.  Use a setInterval()/setTimeout() loop instead
+            * if it matters.
+            */
+            for (var i = 0; i < 5; i++) {
+                    /* On for 1 second */
+                    rpio.write(16, rpio.HIGH);
+                    rpio.sleep(1);
+
+                    /* Off for half a second (500ms) */
+                    rpio.write(16, rpio.LOW);
+                    rpio.msleep(500);
+            }
+
+
         }
       
     }
